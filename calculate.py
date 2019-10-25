560d7b119409f5fa69b83637fbcd906133c613fa
evangallagher <evga7768@colorado.edu>
2019-10-24 20:29:06 -0600

    Add argparse to calculate.py

--- calculate.py
+++ calculate.py
@@ -1,5 +1,23 @@
-from math_lib import div, add, mult
+import math_lib
+import argparse
 
-print(div(9, 3))
-print(add(9, 3))
-print(mult(3,3))
+def main():
+    parser = argparse.ArgumentParser(description='Divide and add two variables')
+
+    parser.add_argument('-a',
+                        type=int,
+                        help='Variable a',
+                        required=True)
+
+    parser.add_argument('-b',
+                        type=int,
+                        help='Variable b',
+                        required=True)
+
+    args = parser.parse_args()
+
+    print(str(args.a) + ' + ' + str(args.b) + ' = ' + str(math_lib.add(args.a, args.b)))
+    print(str(args.a) + ' / ' + str(args.b) + ' = ' + str(math_lib.div(args.a, args.b)))
+
+if __name__ == '__main__':
+    main()
